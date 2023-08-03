4ef033b7c8f6e8e976b0c255cc7f3198562ad256
SiriusMklain <sergeimakarenko5991@gmail.com>
2023-08-03 14:32:20 +0300

    added docker

--- ed_site_autopilot/settings.py
+++ ed_site_autopilot/settings.py
@@ -123,14 +123,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static')
+#     ]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = "/static/"
 
 # STATIC_URL = "static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
